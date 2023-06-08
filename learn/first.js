6d6d59c852d6ec404ef0296634722900336f5dd6
ParthMishra20 <124814034+ParthMishra20@users.noreply.github.com>
2023-06-08 16:36:58 +0530

    Update first.js

--- learn/first.js
+++ learn/first.js
@@ -1,13 +1,31 @@
-console.log("This program adds 1,2 and 54")
+/*console.log("This program adds 1,2 and 54")
 function add(a,b,c){
     return a+b+c;
 }
 let a = add(1,2,54)
-console.log(a)
+console.log(a)*/
 
 //variables are just like containers 
 
-let a2=67
+/*let a2=67
 console.log(a2)
 a2='parth'
-console.log(a2)     
+console.log(a2)*/     
+
+//var is used to declare a variable,but ye pehle use karte the jab se es6 se pehle use karte the var ko 
+
+console.log("Javascript is a dynamic language")
+var b='parth'
+{
+    var b= 'parth2'
+    console.log(b)
+}
+console.log(b)
+//diff between var and let
+console.log("Javascript is a dynamic language")
+let c='parth'
+{
+    let c= 'parth2'
+    console.log(c)
+}
+console.log(c)
