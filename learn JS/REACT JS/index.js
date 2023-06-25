5f6f21a01184f7c71ba433d733fe14a4814b8620
ParthMishra20 <124814034+ParthMishra20@users.noreply.github.com>
2023-06-25 19:48:12 +0530

    UPDATE

---
+++ learn JS/REACT JS/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import  ReactDOM  from "react-dom"
+const navbar = (
+    <nav>
+        <h1>REACT</h1>
+        <ul>
+            <li>MENU</li>
+            <li>CONTACT</li>
+            <li>ABOUT</li>
+        </ul>
+    </nav>
+)
+ReactDOM.render(navbar,document.getElementById("root"))
