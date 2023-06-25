d6cac747f5a57e3b05420d95098c629d04e68a56
ParthMishra20 <124814034+ParthMishra20@users.noreply.github.com>
2023-06-25 19:25:58 +0530

    Update index.js

--- learn/REACT 1.js/index.js
+++ learn/REACT 1.js/index.js
@@ -1,2 +1,19 @@
-ReactDOM.render(<p>REACT</p>,
+// ReactDOM.render(<h1>This is a way to make React Program</h1>,document.getElementById("root"))
+
+
+
+// const h1 = document.createElement("h1")
+// h1.textContent = "This is an another way to make a program"
+// h1.className = "header"
+// document.getElementById("root").append(h1)
+
+
+//JSX
+const page=(<div>
+<h1 classname="header">This is JSX</h1>
+<p>This is par</p>
+</div>)
+console.log(page)
+ReactDOM.render(
+   page,
 document.getElementById("root"))
